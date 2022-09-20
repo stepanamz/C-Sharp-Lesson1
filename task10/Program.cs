cadc648a43d31eafadfcf3383e652cc8314aa1fa
stepanamz <stepanamz@mail.ru>
2022-09-21 00:30:27 +0700

    Выполнил ДЗ - 2 урока по c#

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+if ((number < 100) || (number > 999))
+    Console.WriteLine("Это не трёх значное число, попробуй еще раз");
+
+else {
+    number = (number / 10) % 10 ;
+    Console.WriteLine($"Вторая цифра =  {number} ");
+
+}
---
+++ task13/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+int number3 = 0;
+
+if (number<0) 
+    number = -number;
+if (number<100)
+    Console.WriteLine("Третий цифры нету");
+
+else {
+    while (number > 1000)
+    {
+        number=number/10;
+    }
+    number3 = number%10;
+    Console.WriteLine($"Третья цифра заданного числа = {number3}");
+}
+
+
+
+
+
+
+
+
+
+
+
